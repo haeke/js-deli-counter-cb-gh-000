f62e79e37fc25239eba22428fed0921a2c0d7507
edwin rivera <waehake@gmail.com>
2018-01-18 00:54:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function takeANumber(katzDeliLine, name) {
     var position = katzDeliLine.indexOf(name) + 1;
     return `Welcome, ${name}. You are number ${position} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+
+}
