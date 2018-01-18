d10cc203e47cac5172ab1ead6699dd51430186fc
edwin rivera <waehake@gmail.com>
2018-01-18 01:22:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(katzDeliLine) {
     return 'The line is currently empty.';
   } else {
       for (var i = 0; i < katzDeliLine.length; i++) {
-        temparr.push((i + 1) '. ' + katzDeliLine[i]);
+        temparr.push((i + 1)  + '. ' + katzDeliLine[i]);
       }
       return 'The line is currently ' + temparr.join(', ');
   }
