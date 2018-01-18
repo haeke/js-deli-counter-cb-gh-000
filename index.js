10e2e9f1e033676843e42afe61fe4c8c2a146919
edwin rivera <waehake@gmail.com>
2018-01-18 00:57:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine[0] === undefined) {
     return 'There is nobody waiting to be served!';
   } else {
-    return `Currently serving ` + katzDeliLine.shift() + '.';
+    return `Currently serving ` + katzDeliLine.shift() + ``.`;
   }
 }
