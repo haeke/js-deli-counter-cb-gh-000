ff52448293e88752c7ae62b3f22c7b9494c4314f
edwin rivera <waehake@gmail.com>
2018-01-18 00:49:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   if (katzDeliLine.length > 0) {
     katzDeliLine.push(name);
-    var position = katzDeliLine.findIndex(name);
+    var position = katzDeliLine.indexOf(name) + 1;
     return position;
   }
 }
