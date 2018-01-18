ae11159fd3b2f729912f91730d52487ee128e8b8
edwin rivera <waehake@gmail.com>
2018-01-18 00:45:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   if (katzDeliLine.length > 0) {
     katzDeliLine.push(name);
+    var position = katzDeliLine.hasIndex(name);
   }
 }
